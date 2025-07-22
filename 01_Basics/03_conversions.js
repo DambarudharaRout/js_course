165d804a3eee6c0d143e45af449606325889c25e
Dambarudhara Rout <151648525+DambarudharaRout@users.noreply.github.com>
2025-07-22 13:42:45 +0000

    operators

---
+++ 01_Basics/03_conversions.js
@@ -0,0 +1,20 @@
+let score = "33"
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+
+let str1 = "Hello";
+let str2 = " Hitesh"
+console.log(str1+str2)
+
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
