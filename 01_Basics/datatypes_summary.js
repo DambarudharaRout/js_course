43cdd5f8b7df58b06122e457ac1521fa014850bc
Dambarudhara Rout <151648525+DambarudharaRout@users.noreply.github.com>
2025-07-22 14:28:35 +0000

    datatype summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,19 @@
+// symbol datatype
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id == anotherid)
+
+//  array
+const heros = ["Shaktiman", "naagraj", "doga"];
+
+//object
+let myObj = {
+    name: "Hitesh",
+    age: 22
+};
+
+//functions
+const myFunction = function(){
+    console.log("Hello World!!");
+}
+console.log(typeof myFunction)
