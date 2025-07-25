50df51a850d0ed83ddbfc6cf5f769b609f77172f
Dambarudhara Rout <151648525+DambarudharaRout@users.noreply.github.com>
2025-07-25 11:05:58 +0000

    strings intro.

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "Hitesh"
+const repoCount = 50
+//console.log(name + repoCount + "value")
+
+console.log(`Hello my name is ${"Dambarudhara"} and my repocount is ${50}`);
+
+// string declaration
+const gameName = new String("Hitesh")
+console.log(gameName)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
