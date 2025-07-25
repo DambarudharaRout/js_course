cdb2812fece7bad9832d39e58dcbcfd69846c217
Dambarudhara Rout <151648525+DambarudharaRout@users.noreply.github.com>
2025-07-25 14:56:11 +0000

    datatyeps

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -17,3 +17,28 @@ const myFunction = function(){
     console.log("Hello World!!");
 }
 console.log(typeof myFunction)
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// STACKS & HEAPS
+// STACK -> for primitive datatype (Null, Undefined, Symbol, BigInt, Number, String, Boolean)
+// HEAP -> for non-primitive datatype (Array, Object, Function)
+
+// example of heap storage
+let UserOne = {
+    email: "dam@gmail.com",
+    upi: "dam@ybl"
+};
+
+let UserTwo = UserOne;
+UserTwo.email = "dam1622@gmail.com";
+console.log(UserOne.email);
+console.log(UserTwo.email);
+
+// example of stack storage
+let myYoutube = "dam123"
+let anotherYt = myYoutube;
+anotherYt = "damdam";
+console.log(myYoutube);
+console.log(anotherYt);
+
+
