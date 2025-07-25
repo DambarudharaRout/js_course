d36bdcf17adfed119a3152a68f6ca3a324362194
Dambarudhara Rout <151648525+DambarudharaRout@users.noreply.github.com>
2025-07-25 14:55:50 +0000

    Basic maths and number

---
+++ 01_Basics/06_nums_maths.js
@@ -0,0 +1,21 @@
+// const score = 400
+// console.log(score)
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// const otherNumber = 23.2782
+// console.log(otherNumber.toPrecision(3));
+// const numberone = 1124.5678
+// console.log(numberone.toPrecision(2))
+
+// const numberTwo = 1000000000
+// console.log(numberTwo.toLocaleString('en-IN'));
+
+// +++++++++++++++ MATHS +++++++++++++
+console.log(Math)
+console.log(Math.abs(-3));
+console.log(Math.round(4.8)) // >=(.5) => choose upper value otherwise lower value
+ 
+
