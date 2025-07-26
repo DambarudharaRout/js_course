5156e3924862af96e3a6e5bf0c6409689609a61a
Dambarudhara Rout <151648525+DambarudharaRout@users.noreply.github.com>
2025-07-26 14:53:45 +0000

    Arrays Intro.

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,29 @@
+// const myArr = [2,5,1,8,3]
+// const myheroes = ["Shaktiman", "Nagraj", "Dam"]
+
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+// Array methods
+// myArr.push(6)
+// console.log(myArr)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr)
+
+// const newArr = myArr.join()
+// console.log(newArr)
+
+
+// slice & splice
+const myArr = [0,1,2,3,4,5]
+const n1 = myArr.slice(1,3)
+console.log(n1)
+console.log(myArr)
+
+const n2 = myArr.splice(1,3)
+console.log(n2)
+console.log(myArr)
+
