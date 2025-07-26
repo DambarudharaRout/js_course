f0faf767e8f97e6c678f475ece9fd8ece287a67b
Dambarudhara Rout <151648525+DambarudharaRout@users.noreply.github.com>
2025-07-26 13:39:42 +0000

    Date & Time intro.

--- 01_Basics/07_Dates_Time.js
+++ 01_Basics/07_Dates_Time.js
@@ -1,6 +1,20 @@
-let myDate = new Date()
-console.log(myDate.toString())
-console.log(myDate.toDateString())
-console.log(myDate.toJSON())
-console.log(myDate.toLocaleDateString())
-console.log(myDate.toLocaleTimeString())
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+
+let myCreatedDate = new Date("07-26-2025");
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+//to convert into seconds 
+console.log(Math.floor(myCreatedDate.getTime()/1000))
+
+let newDate = new Date()
+console.log(newDate.getDay())
+console.log(newDate.getMonth()+1)
+
+
+newDate.toLocaleString()
